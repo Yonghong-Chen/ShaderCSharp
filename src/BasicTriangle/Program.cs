522648ffe9353e379f6bf9350f088df13d7babd7
Yonghong-Chen <66941502+Yonghong-Chen@users.noreply.github.com>
2022-08-14 01:27:13 +1000

    fixed saving

--- src/BasicTriangle/Program.cs
+++ src/BasicTriangle/Program.cs
@@ -183,7 +183,7 @@ namespace BasicTriangle
             base.OnResize(e);
         }
 
-        protected override void OnRenderFrame(FrameEventArgs e)
+        void DrawFrame()
         {
             // Clear the color buffer.
             GL.Clear(ClearBufferMask.ColorBufferBit);
@@ -203,13 +203,17 @@ namespace BasicTriangle
             // This draws the triangle.
             GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 4);
 
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            DrawFrame();
             // Swap the front/back buffers so what we just rendered to the back buffer is displayed in the window.
             Context.SwapBuffers();
             base.OnRenderFrame(e);
         }
         void ResizeClient(int width, int height)
         {
-            this.Size = new System.Drawing.Size(width, height);
+            this.ClientSize = new System.Drawing.Size(width, height);
         }
         void ExitProgram()
         {
@@ -219,14 +223,44 @@ namespace BasicTriangle
         {
             int width = this.Width;
             int height = this.Height;
+
+            var glError = GL.GetError();
+            //int fbo = GL.GenFramebuffer();
+            //int texture = GL.GenTexture();
+            //GL.BindTexture(TextureTarget.Texture2D, texture);
+
+            //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMagFilter.Nearest);
+            //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+            //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+            //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
+            //GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba32f, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, IntPtr.Zero);
+
+            //glError = GL.GetError();
+
+            //GL.BindFramebuffer(FramebufferTarget.Framebuffer, fbo);
+            //GL.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, texture, 0);
+            ////GL.DrawBuffer(DrawBufferMode.ColorAttachment0);
+            ////GL.NamedFramebufferDrawBuffer(fbo, DrawBufferMode.ColorAttachment0);
+            //GL.BindFramebuffer(FramebufferTarget.Framebuffer, fbo);
+
+            //var code = GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer);
+            //DrawFrame();
             using (Bitmap bitmap = new Bitmap(this.Width, this.Height))
             {
                 System.Drawing.Imaging.BitmapData bits = bitmap.LockBits(new Rectangle(0, 0, width, height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-                GL.ReadPixels(0, 0, this.Width, this.Height, OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, bits.Scan0);
+                glError = GL.GetError();
+                GL.ReadPixels(0, 0, width, height, OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, bits.Scan0);
+                glError = GL.GetError();
                 bitmap.UnlockBits(bits);
                 //bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
                 bitmap.Save(path, System.Drawing.Imaging.ImageFormat.Png);
             }
+            //glError = GL.GetError();
+            //GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+
+            //GL.BindTexture(TextureTarget.Texture2D, 0);
+            //GL.DeleteFramebuffer(fbo);
+            //GL.DeleteTexture(texture);
         }
         void CompileCode(string shaderCode)
         {
@@ -275,7 +309,7 @@ namespace BasicTriangle
         }";
             string standardHeaderFront =
                 @"#version 430
-		layout(location = 0) out vec4 fragColor;
+		out vec4 fragColor;
 
         in vec2 texPos;
 
@@ -298,9 +332,7 @@ namespace BasicTriangle
         void main() {
 
         mainImage(fragColor, texPos.xy);
-
         fragColor.a = fragColor.a * iOpacity;
-
         }
 ";
 
@@ -334,25 +366,33 @@ namespace BasicTriangle
             m_iTime = newTime;
             int loc;
             loc = GL.GetUniformLocation(ShaderProgram, "iOpacity");
-            GL.Uniform1(loc, 1.0);
+            if (loc != -1)
+                GL.Uniform1(loc, 1.0f);
             loc = GL.GetUniformLocation(ShaderProgram, "iResolution");
-            GL.Uniform3(loc, (float)Width, (float)Height, 1.0f); // last paramter is pixel ratio, eg 4:3, 16:9, we set it to 1
+            if (loc != -1)
+                GL.Uniform3(loc, (float)Width, (float)Height, 1.0f); // last paramter is pixel ratio, eg 4:3, 16:9, we set it to 1
 
             loc = GL.GetUniformLocation(ShaderProgram, "iTime");
-            GL.Uniform1(loc, m_iTime); //iTime
+            if (loc != -1)
+                GL.Uniform1(loc, m_iTime); //iTime
 
             loc = GL.GetUniformLocation(ShaderProgram, "iTimeDelta");
-            GL.Uniform1(loc, m_iTimeDelta); //time Delta
+            if (loc != -1)
+                GL.Uniform1(loc, m_iTimeDelta); //time Delta
 
             float frameDuration = 1.0f / 30.0f;
+            
             m_iFrame = (int) (m_iTime / frameDuration);
 
             loc = GL.GetUniformLocation(ShaderProgram, "iFrame");
-            GL.Uniform1(loc, m_iFrame); //frame
+            if (loc != -1)
+                GL.Uniform1(loc, m_iFrame); //frame
             
             loc = GL.GetUniformLocation(ShaderProgram, "iMouse");
-            GL.Uniform4(6, 0.0, 0.0, 0.0f, 0.0f);
+            if (loc != -1)
+                GL.Uniform4(6, 0.0f, 0.0f, 0.0f, 0.0f);
 
+            var GlErrorCode = GL.GetError();
             foreach(iChannel channel in this.m_iChannels)
             {
                 string name = string.Format("iChannel{0}", channel.id);
@@ -372,6 +412,7 @@ namespace BasicTriangle
                 GL.ActiveTexture(units[channel.id]);
                 GL.BindTexture(TextureTarget.Texture2D, channel.textureID);
                 GL.Uniform1(GL.GetUniformLocation(ShaderProgram, name), channel.id);
+                GlErrorCode = GL.GetError();
             }
         }
         [STAThread]
